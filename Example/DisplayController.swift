7a0889b63ad9222ed66965c59ccd8bfeb120b642
Leo Joseph <58416454+ts-leojoseph-r@users.noreply.github.com>
2020-02-17 15:42:01 +0900

    Feature: Sample app integration with displayer module (#23)
    * Feature: Sample app integration with displayer module
    
    * Updated changes - PR feedback
    
    * Removed DispatchQueue block code

---
+++ Example/DisplayController.swift
@@ -0,0 +1,25 @@
+import UIKit
+import MiniApp
+
+class DisplayController: UIViewController {
+    var miniAppInfo: MiniAppInfo?
+
+    override func viewDidLoad() {
+        guard let info = miniAppInfo else {
+            return
+        }
+        self.showProgressIndicator()
+        MiniApp.create(appInfo: info) { (result) in
+            switch result {
+            case .success(let miniAppDisplay):
+                self.dismissProgressIndicator()
+                let view = miniAppDisplay.getMiniAppView()
+                view.frame = self.view.bounds
+                self.view.addSubview(view)
+            case .failure(let error):
+                self.displayErrorAlert(title: "Error", message: "Downloading failed, please try again later", dismissController: true)
+                print("Errored: ", error.localizedDescription)
+            }
+        }
+    }
+}
---
+++ MiniApp/Progress+UIViewController.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+extension UIViewController {
+
+    func showProgressIndicator() {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: nil, message: "Please wait...", preferredStyle: .alert)
+            let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
+            loadingIndicator.hidesWhenStopped = true
+            loadingIndicator.style = UIActivityIndicatorView.Style.gray
+            loadingIndicator.startAnimating()
+            alert.view.addSubview(loadingIndicator)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+
+    func dismissProgressIndicator() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+            self.dismiss(animated: true, completion: nil)
+        })
+    }
+
+    func displayErrorAlert(title: String, message: String, dismissController: Bool) {
+        DispatchQueue.main.asyncAfter(deadline: .now()) {
+            self.dismiss(animated: true, completion: {
+                let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { (_: UIAlertAction!) in
+                    if dismissController {
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                }))
+                self.present(alert, animated: true, completion: nil)
+            })
+        }
+    }
+}
