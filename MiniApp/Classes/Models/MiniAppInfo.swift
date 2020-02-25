d83c5f80161a2481233417154035dc6be9381238
PM Villa <ts-pierremiche.villa@rakuten.com>
2020-02-25 17:49:27 +0900

    refactor: add coding keys
    add coding keys so we we can call it what we want internally

--- MiniApp/Classes/Models/MiniAppInfo.swift
+++ MiniApp/Classes/Models/MiniAppInfo.swift
@@ -4,9 +4,21 @@ public struct MiniAppInfo: Decodable {
     public var displayName: String
     public var icon: URL
     internal var version: Version
+    
+    private enum CodingKeys : String, CodingKey {
+        case id = "id",
+        displayName = "displayName",
+        icon = "icon",
+        version = "version"
+    }
 }
 
 public struct Version: Decodable {
     public var versionTag: String
     public var versionId: String
+    
+    private enum CodingKeys : String, CodingKey {
+        case versionTag = "versionTag",
+        versionId = "versionId"
+    }
 }
