48b279c1f7577a9d559d35aefc3f0a8f0f966489
PM Villa <ts-pierremiche.villa@rakuten.com>
2020-02-25 17:49:27 +0900

    refactor: add coding keys
    add coding keys so we we can call it what we want internally

--- MiniApp/Classes/Models/ManifestResponse.swift
+++ MiniApp/Classes/Models/ManifestResponse.swift
@@ -1,4 +1,8 @@
 /// Mini App Manifest information
 internal struct ManifestResponse: Decodable {
-    var files: [String]
+    var manifest: [String]
+    
+    private enum CodingKeys : String, CodingKey {
+        case manifest = "manifest"
+    }
 }
