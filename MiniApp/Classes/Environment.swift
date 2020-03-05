216e16ac79f4a94da93b76d9d768e82ff1e61515
PM Villa <ts-pierremiche.villa@rakuten.com>
2020-03-05 11:22:47 +0900

    refactor: changed String keys by an enum

--- MiniApp/Classes/Environment.swift
+++ MiniApp/Classes/Environment.swift
@@ -4,6 +4,13 @@ internal protocol EnvironmentProtocol {
 }
 
 internal class Environment {
+    enum keys: String {
+        case applicationIdentifier = "RASApplicationIdentifier",
+        version = "CFBundleShortVersionString",
+        subscriptionKey = "RASProjectSubscriptionKey",
+        endpoint = "RMAAPIEndpoint"
+    }
+
     let bundle: EnvironmentProtocol
 
     var customUrl: String?
@@ -16,19 +23,19 @@ internal class Environment {
     }
 
     var appId: String {
-        return customAppId ?? bundle.value(for: "RASApplicationIdentifier" as String) ?? bundle.valueNotFound
+        return customAppId ?? bundle.value(for: keys.applicationIdentifier.rawValue) ?? bundle.valueNotFound
     }
 
     var appVersion: String {
-        return customAppVersion ?? bundle.value(for: "CFBundleShortVersionString" as String) ?? bundle.valueNotFound
+        return customAppVersion ?? bundle.value(for: keys.version.rawValue) ?? bundle.valueNotFound
     }
 
     var subscriptionKey: String {
-        return customSubscriptionKey ?? bundle.value(for: "RASProjectSubscriptionKey") ?? bundle.valueNotFound
+        return customSubscriptionKey ?? bundle.value(for: keys.subscriptionKey.rawValue) ?? bundle.valueNotFound
     }
 
     var baseUrl: URL? {
-        guard let endpointUrlString = (self.customUrl ?? bundle.value(for: "RMAAPIEndpoint")) else {
+        guard let endpointUrlString = (self.customUrl ?? bundle.value(for: keys.endpoint.rawValue)) else {
             Logger.e("Ensure RMAAPIEndpoint value in plist is valid")
             return nil
         }
