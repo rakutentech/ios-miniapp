c687bf26d79981d0e72df98d3768f220adc1d9de
Leo Joseph <58416454+ts-leojoseph-r@users.noreply.github.com>
2020-03-16 12:37:00 +0900

    Refactor - Removed logger class and updated few testcases (#38)
    * Refactor - Removed logger class and updated few testcases
    
    * Updated description in testcase
    
    * Renamed logger class

---
+++ Tests/Unit/UrlParserTests.swift
@@ -0,0 +1,24 @@
+import Quick
+import Nimble
+@testable import MiniApp
+
+class UrlParserTests: QuickSpec {
+    override func spec() {
+        describe("Url parser") {
+            context("when parseForFileDirectory is called with valid url") {
+                it("will returns the path to store the file") {
+                    let urlString = "https://www.example.com/version/rak0123/img/onload/file.png"
+                    let path = UrlParser.parseForFileDirectory(with: urlString)
+                    expect(path).toEventually(equal("img/onload/file.png"))
+                }
+            }
+            context("when parseForFileDirectory is called with invalid url") {
+                it("will return invalid path") {
+                    let urlString = "https://www.example.com/mini-ver/rak0123/img/onload/file.png"
+                    let path = UrlParser.parseForFileDirectory(with: urlString)
+                    expect(path).toEventuallyNot(equal("img/onload/file.png"))
+                }
+            }
+        }
+    }
+}
