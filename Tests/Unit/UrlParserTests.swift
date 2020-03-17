9f637af8ab3e50e093efd386540a84f9e361daa5
Leo Joseph <ts-leojoseph.r@rakuten.com>
2020-03-17 23:04:22 +0900

    Updated Urlparser to parse using latest separation key

--- Tests/Unit/UrlParserTests.swift
+++ Tests/Unit/UrlParserTests.swift
@@ -5,18 +5,25 @@ import Nimble
 class UrlParserTests: QuickSpec {
     override func spec() {
         describe("Url parser") {
-            context("when parseForFileDirectory is called with valid url") {
+            context("when getFileStoragePath is called with valid url") {
                 it("will returns the path to store the file") {
-                    let urlString = "https://www.example.com/version/rak0123/img/onload/file.png"
-                    let path = UrlParser.parseForFileDirectory(with: urlString)
+                    let urlString = "https://www.example.com/map-published/mini-abc/ver-abc/img/onload/file.png"
+                    let path = UrlParser.getFileStoragePath(from: urlString)
                     expect(path).toEventually(equal("img/onload/file.png"))
                 }
             }
-            context("when parseForFileDirectory is called with invalid url") {
-                it("will return invalid path") {
+            context("when getFileStoragePath is called with valid url with no separator") {
+                it("will return nil") {
                     let urlString = "https://www.example.com/mini-ver/rak0123/img/onload/file.png"
-                    let path = UrlParser.parseForFileDirectory(with: urlString)
-                    expect(path).toEventuallyNot(equal("img/onload/file.png"))
+                    let path = UrlParser.getFileStoragePath(from: urlString)
+                    expect(path).toEventually(beNil())
+                }
+            }
+            context("when getFileStoragePath is called with invalid url") {
+                it("will return nil") {
+                    let urlString = "https://www.example.com/map-published/"
+                    let path = UrlParser.getFileStoragePath(from: urlString)
+                    expect(path).toEventually(beNil())
                 }
             }
         }
