50fb61bf4a05d201438ac012187e1fb2527496be
Leo Joseph <ts-leojoseph.r@rakuten.com>
2020-04-08 16:26:10 +0900

    Added documentation for getUniqueId

--- MiniApp/Classes/Protocols/MiniAppMessageProtocol.swift
+++ MiniApp/Classes/Protocols/MiniAppMessageProtocol.swift
@@ -3,5 +3,7 @@ Public Protocol that will be used by the Mini App to communicate
  with the Native implementation
 */
 public protocol MiniAppMessageProtocol {
+
+    /// Interface that should be implemented to return alphanumeric string that uniquely identifies a device.
     func getUniqueId() -> String
 }
