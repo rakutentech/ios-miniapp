ec88a0a08eed2dc765076962b11aee42c4e881ff
Leo Joseph <58416454+ts-leojoseph-r@users.noreply.github.com>
2020-04-15 17:34:24 +0900

    Feature/ios javascript interface implementation (#55)
    * SDK and Mini App interface bridge implementation
    
    * Added testcases
    
    * Swiftlint changes
    
    * Renamed messageInterface to hostAppMessageDelegate
    
    * Changes based on feedback comments
    * Spelling changes
    * Protocol set as weak
    * Default param as pod bundle
    
    Co-authored-by: Pierre-Michel Villa <ts-pierremiche.villa@rakuten.com>

--- MiniApp/Classes/Protocols/MiniAppMessageProtocol.swift
+++ MiniApp/Classes/Protocols/MiniAppMessageProtocol.swift
@@ -2,7 +2,7 @@
 Public Protocol that will be used by the Mini App to communicate
  with the Native implementation
 */
-public protocol MiniAppMessageProtocol {
+public protocol MiniAppMessageProtocol: class {
 
     /// Interface that should be implemented to return alphanumeric string that uniquely identifies a device.
     func getUniqueId() -> String
