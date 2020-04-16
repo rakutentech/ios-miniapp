260dc974072e9b7d43580af11d351f451ed678b3
Nguyen, Minh | MTSD <ts-minh.nguyen@rakuten.com>
2020-04-16 12:29:46 +0900

    refactor: export default in index

--- js-miniapp-sdk/src/index.ts
+++ js-miniapp-sdk/src/index.ts
@@ -3,6 +3,4 @@ const MiniApp = require('./miniapp.js');
 
 const miniAppInstance = new MiniApp();
 
-export = function getUniqueId(): Promise<string> {
-  return miniAppInstance.getUniqueId();
-};
+export default miniAppInstance
