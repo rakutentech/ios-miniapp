2515e54527f8c192bf0410b9a3c867573824b485
Pierre-Michel Villa <ts-pierremiche.villa@rakuten.com>
2020-04-17 17:10:21 +0900

    Feature/mini 858 (#56)
    * refactor : Can open unpublished version as long as the list isn't refreshed
    
    * removed unnecessary foundation framework
    
    Co-authored-by: Leo Joseph <58416454+ts-leojoseph-r@users.noreply.github.com>

--- Example/DisplayController.swift
+++ Example/DisplayController.swift
@@ -1,9 +1,8 @@
 import UIKit
 import MiniApp
 
-class DisplayController: UIViewController, MiniAppMessageProtocol {
+class DisplayController: UIViewController {
 
-    var miniAppInfo: MiniAppInfo?
     var config: MiniAppSdkConfig?
 
     override func viewWillAppear(_ animated: Bool) {
@@ -11,31 +10,15 @@ class DisplayController: UIViewController, MiniAppMessageProtocol {
     }
 
     override func viewDidAppear(_ animated: Bool) {
-        guard let controller = self.navigationController as? DisplayNavigationController, let info = controller.miniAppInfo else {
+        guard let controller = self.navigationController as? DisplayNavigationController, let info = controller.miniAppInfo, let miniAppDisplay = controller.miniAppDisplay else {
             return
         }
 
-        self.showProgressIndicator()
         self.title = info.displayName
-        MiniApp.shared(with: config).create(appInfo: info, completionHandler: { (result) in
-            switch result {
-            case .success(let miniAppDisplay):
-                self.dismissProgressIndicator()
-                let view = miniAppDisplay.getMiniAppView()
-                view.frame = self.view.bounds
-                self.view.addSubview(view)
-            case .failure(let error):
-                self.displayAlert(title: NSLocalizedString("error_title", comment: ""), message: NSLocalizedString("error_miniapp_download_message", comment: ""), dismissController: true)
-                print("Errored: ", error.localizedDescription)
-            }
-        }, messageInterface: self)
-    }
 
-    func getUniqueId() -> String {
-        guard let deviceId = UIDevice.current.identifierForVendor?.uuidString else {
-            return ""
-        }
-        return deviceId
+        let view = miniAppDisplay.getMiniAppView()
+        view.frame = self.view.bounds
+        self.view.addSubview(view)
     }
 
     @IBAction func done(_ sender: UIBarButtonItem) {
