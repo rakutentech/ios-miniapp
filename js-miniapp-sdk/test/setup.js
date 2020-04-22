55106ddbe021edc7274c30aee53f58f6b8d2f553
Cory Caywood <cory@floatinginspace.net>
2020-04-22 12:56:35 +0900

    tests: Change test setup so it will output to both console and xml
    Also added some additional configuration for using chaip-as-promised

---
+++ js-miniapp-sdk/test/setup.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const chai = require('chai');
+
+chai.use(require('chai-as-promised'))
+chai.use(require('sinon-chai'));
--- js-miniapp-sdk/tests/test.spec.ts
+++
@@ -1,7 +0,0 @@
-// import { expect } from 'chai';
-//
-// describe('sample test', () => {
-//   it('sample add', () => {
-//     expect(2 + 2).to.equal(4);
-//   });
-// });
