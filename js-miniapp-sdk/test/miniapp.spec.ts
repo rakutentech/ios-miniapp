1d6119602b9ecb339d81e37a1d8b629bb27054c9
Cory Caywood <cory@floatinginspace.net>
2020-04-22 12:57:25 +0900

    tests: Add unit test for MiniApp.getUniqueId

---
+++ js-miniapp-sdk/test/miniapp.spec.ts
@@ -0,0 +1,27 @@
+/* tslint:disable:no-any */
+
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import MiniApp from '../src/miniapp';
+
+const window: any = {};
+(global as any).window = window;
+
+describe('getUniqueId', () => {
+  before(() => {
+    window.MiniAppBridge = {
+      getUniqueId: sinon.stub(),
+    };
+  });
+
+  it('should retrieve the unique id from the Mini App Bridge', () => {
+    window.MiniAppBridge.getUniqueId.resolves('test_mini_app_id');
+
+    const miniApp = new MiniApp();
+
+    return expect(miniApp.getUniqueId()).to.eventually.equal(
+      'test_mini_app_id'
+    );
+  });
+});
