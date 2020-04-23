acbd8f8b3425ab1397587a4f5f1778423575f784
Cory Caywood <cory@floatinginspace.net>
2020-04-23 15:02:20 +0900

    build: (sdk) Add bundling w/ browserify

--- js-miniapp-sdk/src/index.ts
+++ js-miniapp-sdk/src/index.ts
@@ -2,5 +2,4 @@ import { MiniApp } from './miniapp';
 
 const miniAppInstance = new MiniApp();
 
-// tslint:disable-next-line:no-default-export
-export default miniAppInstance;
+export = miniAppInstance;
