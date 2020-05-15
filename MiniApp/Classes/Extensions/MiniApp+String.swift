ce69a674cd6a63f930b13f3044f7cd49e158cf52
Leo Joseph <58416454+ts-leojoseph-r@users.noreply.github.com>
2020-05-15 12:19:27 +0900

    Fix to remove the extra backslash (#76)

---
+++ MiniApp/Classes/Extensions/MiniApp+String.swift
@@ -0,0 +1,6 @@
+extension String {
+    func deletingPrefix(_ prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return self }
+        return String(self.dropFirst(prefix.count))
+    }
+}
