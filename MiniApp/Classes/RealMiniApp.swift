2e6350a6f2f0dd8895e0133da97afd5d64f8f08f
Leo Joseph <ts-leojoseph.r@rakuten.com>
2020-06-09 12:20:09 +0900

    Check Mini app latest version is available and download

--- MiniApp/Classes/RealMiniApp.swift
+++ MiniApp/Classes/RealMiniApp.swift
@@ -31,7 +31,33 @@ internal class RealMiniApp {
         return miniAppInfoFetcher.getInfo(miniAppId: miniAppId, apiClient: self.miniAppClient, completionHandler: completionHandler)
     }
 
+    /// For a given Miniapp info object, this method will check whether the version id is the latest one with the platform.
+    /// If the versions doesn't match it will start downloading the latest version, if the versions match the same object
+    /// will be passed on to Downloader class (which will check whether the mini app is downloaded already if not, it will download)
+    /// - Parameters:
+    ///   - appInfo: Miniapp Info object
+    ///   - completionHandler: Completion Handler that needed to pass back the MiniAppDisplayProtocol
+    ///   - messageInterface: Miniapp communication protocol object.
     func createMiniApp(appInfo: MiniAppInfo, completionHandler: @escaping (Result<MiniAppDisplayProtocol, Error>) -> Void, messageInterface: MiniAppMessageProtocol? = nil) {
+        getMiniApp(miniAppId: appInfo.id) { (result) in
+            switch result {
+            case .success(let responseData):
+                if appInfo.version.versionId != responseData.version.versionId {
+                    self.downloadMiniApp(appInfo: responseData, completionHandler: completionHandler)
+                    return
+                }
+                self.downloadMiniApp(appInfo: appInfo, completionHandler: completionHandler)
+            case .failure(let error):
+                completionHandler(.failure(error))
+        }}
+    }
+
+    /// Download Mini app for a given Mini app info object
+    /// - Parameters:
+    ///   - appInfo: Miniapp Info object
+    ///   - completionHandler: Completion Handler that needed to pass back the MiniAppDisplayProtocol
+    ///   - messageInterface: Miniapp communication protocol object.
+    func downloadMiniApp(appInfo: MiniAppInfo, completionHandler: @escaping (Result<MiniAppDisplayProtocol, Error>) -> Void, messageInterface: MiniAppMessageProtocol? = nil) {
         return miniAppDownloader.download(appId: appInfo.id, versionId: appInfo.version.versionId) { (result) in
             switch result {
             case .success:
