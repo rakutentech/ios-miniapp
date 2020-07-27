c595b34885553ca4919cf0ca130449511c6b4ffe
Leo Joseph <58416454+ts-leojoseph-r@users.noreply.github.com>
2020-07-27 09:26:41 +0900

    Enable override localizable strings from Host app (#108)
    * Enable override localizable strings from Host app
    
    * Moved to Extension
    
    * Swiftlint
    
    * Updated testcases

--- MiniApp/Classes/Extensions/MiniApp+String.swift
+++ MiniApp/Classes/Extensions/MiniApp+String.swift
@@ -3,4 +3,19 @@ extension String {
         guard self.hasPrefix(prefix) else { return self }
         return String(self.dropFirst(prefix.count))
     }
+
+    /// Function returns the Localized string for a given Key
+    /// If the same key is added in the Host application, then the localizable string from the Host app is returned
+    /// - Returns:Localized String from the Bundle
+    func localizedString(path: String = Bundle(for: MiniApp.self).path(forResource: "Localization", ofType: "bundle")!) -> String {
+        guard let podBundle = Bundle(path: path) else {
+            return self
+        }
+        let defaultValue = NSLocalizedString(self, bundle: podBundle, comment: "")
+        return NSLocalizedString(self,
+                                 tableName: "Localizable",
+                                 bundle: Bundle.main,
+                                 value: defaultValue,
+                                 comment: "")
+    }
 }
---
+++ Tests/Unit/MiniApp+StringTests.swift
@@ -0,0 +1,35 @@
+import Quick
+import Nimble
+@testable import MiniApp
+
+class MiniAppStringTests: QuickSpec {
+
+    override func spec() {
+        describe("Mini app + String extension") {
+            context("when localized key is passed that is available in Pod bundle") {
+                it("will return valid localized string") {
+                    let okLocalizedTitle = "Ok_title".localizedString()
+                    expect(okLocalizedTitle).toEventually(equal("OK"), timeout: 10)
+                }
+            }
+            context("when localized key is passed that is available in Pod bundle") {
+                it("will return valid localized string") {
+                    let cancelLocalizedTitle = "Cancel_title".localizedString()
+                    expect(cancelLocalizedTitle).toEventually(equal("Cancel"), timeout: 10)
+                }
+            }
+            context("when localized key is passed with pod bundle path as empty") {
+                it("will return the key as string") {
+                    let okLocalizedTitle = "Ok_title".localizedString(path: "")
+                    expect(okLocalizedTitle).toEventually(equal("Ok_title"), timeout: 10)
+                }
+            }
+            context("when localized key is passed with pod bundle path as empty") {
+                it("will return the key as string") {
+                    let cancelLocalizedTitle = "Cancel_title".localizedString(path: "")
+                    expect(cancelLocalizedTitle).toEventually(equal("Cancel_title"), timeout: 10)
+                }
+            }
+        }
+    }
+}
