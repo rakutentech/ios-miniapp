50d0b8e6480ac15070b402a993da55582d3ed272
Leo Joseph <ts-leojoseph.r@rakuten.com>
2020-07-28 14:04:27 +0900

    Removed delegate

--- MiniApp/Classes/JavascriptBridge/MiniAppScriptMessageHandler.swift
+++ MiniApp/Classes/JavascriptBridge/MiniAppScriptMessageHandler.swift
@@ -114,7 +114,7 @@ internal class MiniAppScriptMessageHandler: NSObject, WKScriptMessageHandler {
     }
 }
 
-class LocationManager: NSObject, CLLocationManagerDelegate {
+class LocationManager: NSObject {
     let manager: CLLocationManager
     let location: CLLocation?
 
