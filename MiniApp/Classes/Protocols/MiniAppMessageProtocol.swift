5ff538e81867587d3c97af72c527dcecfbe06b1a
Leo Joseph <ts-leojoseph.r@rakuten.com>
2020-10-13 11:09:01 +0900

    feedback changes

--- MiniApp/Classes/Protocols/MiniAppMessageProtocol.swift
+++ MiniApp/Classes/Protocols/MiniAppMessageProtocol.swift
@@ -19,6 +19,18 @@ public protocol MiniAppMessageProtocol: MiniAppUserInfoDelegate {
     func shareContent(info: MiniAppShareContent, completionHandler: @escaping (Result<MASDKProtocolResponse, Error>) -> Void)
 }
 
+/**
+ Public Protocol that will be used by the Mini App to communicate
+ with the Native implementation for User profile related retrieval
+*/
+public protocol MiniAppUserInfoDelegate: class {
+    /// Interface that is used to retrieve the user name from the User Profile
+    func getUserName() -> String?
+
+    /// Interface that is used to retrieve the Image URI
+    func getProfilePhoto() -> String?
+}
+
 public enum MASDKProtocolResponse: String {
     case success = "SUCCESS"
 }
