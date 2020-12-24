dd07264d91184efeb63369f709f386d68c7478b6
PM Villa <pierremichel.villa@rakuten.com>
2020-12-24 10:53:39 +0900

    MiniApp analytics calls and configuration

--- MiniApp/Classes/Extensions/NotificationCenter+RAT.swift
+++ MiniApp/Classes/Extensions/NotificationCenter+RAT.swift
@@ -1,28 +1,16 @@
-internal enum RATEventType: String {
-    case appear
-    case click
-    case custom
-}
-
-internal enum RATEvent: String {
-    case mini_app_host_launch
-    case mini_app_open
-    case mini_app_close
-}
-
 internal extension NotificationCenter {
-    func sendAnalytics(event name: RATEvent, type: RATEventType? = nil, parameters customData: (String,String)...) {
-        sendAnalytics(event: name, type: type, parameters: customData)
+    func sendAnalytics(event: MiniAppRATEvent, type: MiniAppRATEventType? = nil, parameters customData: (String,String)...) {
+        sendAnalytics(event: event, type: type, parameters: customData)
     }
     
-    func sendAnalytics(event name: RATEvent, type: RATEventType? = nil, parameters customData: [(String,String)]? = nil) {
+    func sendAnalytics(event: MiniAppRATEvent, type: MiniAppRATEventType? = nil, parameters customData: [(String,String)]? = nil) {
         var parameters = [String: Codable]()
-        parameters["eventName"] = name.rawValue
+        parameters["eventName"] = event.name()
         parameters["eventData"] = customData?.reduce([String:String]()) { (eventData, param) in
                 var mutableEventData = eventData
-                mutableEventData.updateValue(param.1, forKey: param.0)
+            mutableEventData.updateValue(param.1, forKey: param.0)
                 return mutableEventData
             }
-        self.post(name: Notification.Name("com.rakuten.esd.sdk.events.\((type ?? .custom).rawValue)"), object: parameters)
+        self.post(name: MiniAppAnalytics.notification(type: type ?? event.eType()), object: parameters)
     }
 }
