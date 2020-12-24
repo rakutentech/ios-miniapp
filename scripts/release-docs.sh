f28c9cba7e51e671a1af0b67c6297aa15ea502ca
Cory Caywood <cory@floatinginspace.net>
2020-12-24 12:50:50 +0900

    ci: Move CI scripts into local script files
    This allows us to more easily move between different CI services.
    
    Also moved the jazzy doc settings into the local config file so docs can be generated by just running 'bundle excec jazzy'

---
+++ scripts/release-docs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -ex
+
+# Publishes SDK documentation to Github Pages for a Git tag ($RELEASE_TAG)
+
+# You must set the environent variable $RELEASE_GIT_REMOTE
+# It should be inclue a github token in the format:
+# https://{GITHUB_TOKEN}@github.com/{ORG}/{REPO}.git
+
+git fetch $RELEASE_GIT_REMOTE gh-pages:gh-pages --force
+git checkout -f gh-pages
+
+echo "Copying new docs to gh-pages branch."
+git rm -r '*'
+git add docs
+git mv docs/* ./ -k
+
+echo "Pushing docs for $RELEASE_TAG to gh-pages branch."
+git commit -m "Publish documentation for $RELEASE_TAG [ci skip]"
+git push $RELEASE_GIT_REMOTE gh-pages
