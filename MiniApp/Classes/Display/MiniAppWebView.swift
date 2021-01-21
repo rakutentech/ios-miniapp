45dd71a55cb3adf9d42fafcb28c140136a7b2a60
Leo Joseph <ts-leojoseph.r@rakuten.com>
2021-01-21 19:48:46 +0900

    Remove encoding for all query params

--- MiniApp/Classes/Display/MiniAppWebView.swift
+++ MiniApp/Classes/Display/MiniAppWebView.swift
@@ -14,25 +14,38 @@ internal class MiniAppWebView: WKWebView {
 
     convenience init(miniAppId: String, versionId: String, queryParams: String? = nil) {
         let schemeName = Constants.miniAppSchemePrefix + miniAppId
-        let urlRequest = URLRequest(url: URL(string: schemeName + "://miniapp/" + Constants.rootFileName + MiniAppWebView.getQueryParams(queryParams: queryParams))!)
         let config = MiniAppWebView.defaultConfig()
         config.setURLSchemeHandler(URLSchemeHandler(versionId: versionId), forURLScheme: schemeName)
         self.init(frame: .zero, configuration: config)
-        commonInit(urlRequest: urlRequest)
+        commonInit(urlRequest: Self.getURLRequest(miniAppId: miniAppId, schemeName: schemeName, queryParams: queryParams))
     }
 
     convenience init(miniAppURL: URL, queryParams: String? = nil) {
-        let urlWithQueryParam = miniAppURL.appendingPathComponent(MiniAppWebView.getQueryParams(queryParams: queryParams))
-        let urlRequest = URLRequest(url: urlWithQueryParam)
+        let urlRequest = URLRequest(url: Self.getLoadByURLRequest(miniAppURL: miniAppURL, queryParams: queryParams))
         self.init(frame: .zero, configuration: MiniAppWebView.defaultConfig())
         commonInit(urlRequest: urlRequest)
     }
 
+    private static func getLoadByURLRequest(miniAppURL: URL, queryParams: String?) -> URL {
+        guard let urlWithQueryParam = miniAppURL.appendingPathComponent(Self.getQueryParams(queryParams: queryParams)).absoluteString.removingPercentEncoding else {
+            return miniAppURL
+        }
+
+        return URL(string: urlWithQueryParam)!
+    }
+
+    private static func getURLRequest(miniAppId: String, schemeName: String, queryParams: String?) -> URLRequest {
+        guard let url = URL(string: schemeName + "://miniapp/" + Constants.rootFileName + MiniAppWebView.getQueryParams(queryParams: queryParams)) else {
+            return URLRequest(url: URL(string: schemeName + "://miniapp/" + Constants.rootFileName)!)
+        }
+        return URLRequest(url: url)
+    }
+
     private static func getQueryParams(queryParams: String?) -> String {
-        guard let param = queryParams, let queryString =  param.encodeURLParam() else {
+        guard let paramsString = queryParams, !paramsString.isEmpty else {
             return ""
         }
-        return "?" + queryString
+        return "?" + paramsString.replacingOccurrences(of: " ", with: "%20")
     }
 
     private func commonInit(urlRequest: URLRequest) {
