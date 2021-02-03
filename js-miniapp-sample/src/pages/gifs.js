286b200082048c0db35b6b923f7b793cf9cc5e43
Rajesh-Sundaram <61268229+Rajesh-Sundaram@users.noreply.github.com>
2021-02-03 10:30:45 +0900

    add gif tab (#96)
    * add gif tab
    Co-authored-by: mohanadwalo <ts-mohanad.walo@rakuten.com>

---
+++ js-miniapp-sample/src/pages/gifs.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import { Typography, CardContent, CardMedia, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+import GreyCard from '../components/GreyCard';
+const useStyles = makeStyles((theme) => ({
+  grid: {
+    position: 'relative',
+    top: '28%',
+    paddingBottom: 15,
+  },
+  greyCard: {
+    marginTop: '1rem',
+  },
+  typography: {
+    marginTop: '1rem',
+  },
+}));
+
+function GIFComponent() {
+  const classes = useStyles();
+  const images = [
+    {
+      label: 'Loop Count: Once',
+      iconSrc: require('../assets/images/gif/road.gif'),
+      altLabel: 'road',
+    },
+    {
+      label: 'Loop Count: Infinite',
+      iconSrc: require('../assets/images/gif/road_infinite.gif'),
+      altLabel: 'infinite_road',
+    },
+    {
+      label: 'Loop Count: Infinite (WebP)',
+      // $FlowFixMe
+      iconSrc: require('../assets/images/webp/road.webp'),
+      altLabel: 'infinite_road_webp',
+    },
+  ];
+  return (
+    <Grid
+      container
+      direction="column"
+      justify="flex-start"
+      alignItems="center"
+      className={classes.grid}
+    >
+      {images.map((it, i) => (
+        <React.Fragment item key={i}>
+          <GreyCard height="auto" className={`${classes.greyCard}`}>
+            <Typography className={`app-typography ${classes.typography}`}>
+              {it.label}
+            </Typography>
+            <CardContent>
+              <CardMedia
+                component="img"
+                className={classes.gif}
+                src={it.iconSrc}
+                alt={it.altLabel}
+              />
+            </CardContent>
+          </GreyCard>
+        </React.Fragment>
+      ))}
+    </Grid>
+  );
+}
+
+export default GIFComponent;
--- js-miniapp-sample/src/routes.js
+++ js-miniapp-sample/src/routes.js
@@ -1,34 +1,36 @@
 import React from 'react';
 
+import AttachFileIcon from '@material-ui/icons/AttachFile';
 import ChatIcon from '@material-ui/icons/Chat';
 import FingerprintIcon from '@material-ui/icons/Fingerprint';
+import GifIcon from '@material-ui/icons/Gif';
 import HomeIcon from '@material-ui/icons/Home';
 import LaptopWindowsIcon from '@material-ui/icons/LaptopWindows';
 import LinkIcon from '@material-ui/icons/Link';
+import AdsIcon from '@material-ui/icons/LocalPlay';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import LockOpenIcon from '@material-ui/icons/LockOpen';
+import MediaIcon from '@material-ui/icons/MusicVideo';
 import PersonIcon from '@material-ui/icons/Person';
+import ShareIcon from '@material-ui/icons/Share';
 import StorageIcon from '@material-ui/icons/Storage';
 import VpnKeyIcon from '@material-ui/icons/VpnKey';
-import MediaIcon from '@material-ui/icons/MusicVideo';
-import ShareIcon from '@material-ui/icons/Share';
-import AdsIcon from '@material-ui/icons/LocalPlay';
-import AttachFileIcon from '@material-ui/icons/AttachFile';
 
+import Ads from './pages/ads';
 import AuthToken from './pages/auth-token';
 import TalkToChatBot from './pages/chatbot';
 import FetchCredentials from './pages/fetch-credentials';
+import FileUploader from './pages/file-upload';
+import GifPage from './pages/gifs';
 import Landing from './pages/landing';
 import LocalStorage from './pages/local-storage';
+import Media from './pages/media';
+import Share from './pages/share';
 import UriSchemes from './pages/uri-schemes';
 import UserDetails from './pages/user-details';
 import UuidFetcher from './pages/uuid-sdk';
 import WebLocation from './pages/web-location';
 import WindowActions from './pages/window-actions';
-import Media from './pages/media';
-import Share from './pages/share';
-import Ads from './pages/ads';
-import FileUploader from './pages/file-upload';
 
 const homeItem = [
   {
@@ -118,8 +120,15 @@ const appItems = [
     navLink: '/file_upload',
     component: FileUploader,
   },
+  {
+    icon: <GifIcon />,
+    label: "GIF's & WebP",
+    navLink: '/gifs',
+    component: GifPage,
+  },
 ];
-const navItems: Object[] = homeItem.concat(
+// $FlowFixMe
+const navItems = homeItem.concat(
   appItems.sort((a, b) => a.label.localeCompare(b.label))
 );
 
