b59f4046ed611d92d81e2d7893ca26db7645f51b
Leo Joseph <ts-leojoseph.r@rakuten.com>
2021-04-09 02:54:12 +0900

    Feedback changes

--- MiniApp/Classes/core/Storage/MAManifestCache.swift
+++ MiniApp/Classes/core/Storage/MAManifestCache.swift
@@ -3,44 +3,27 @@ import CommonCrypto
 
 internal class MAManifestCache {
 
-    typealias KeysDictionary = [String: CachedMetaData]
+    typealias KeysDictionary = [String: [String: CachedMetaData]]
     let keychainStore = MiniAppKeyChain(serviceName: .miniAppManifestCache)
-
+    let cacheName = "ManifestCache"
     func saveManifestInfo(forMiniApp appId: String, versionId: String, manifest: CachedMetaData) {
         guard !appId.isEmpty && !versionId.isEmpty else {
             return
         }
-        var keysDic = retrieveAllManifestInfo()
-        if keysDic != nil {
-            keysDic?[appId + "/" + versionId] = manifest
-        } else {
-            keysDic = [appId + "/" + versionId: manifest]
-        }
-
-        if let keys = keysDic {
-            keychainStore.setInfoInKeyChain(keys: keys)
-        }
+        keychainStore.setInfoInKeyChain(keys: [cacheName: [appId + "/" + versionId: manifest]])
     }
 
     func getManifestInfo(forMiniApp appId: String, versionId: String) -> CachedMetaData? {
         guard !appId.isEmpty && !versionId.isEmpty else {
             return nil
         }
-        guard let allKeys = retrieveAllManifestInfo(), let manifestInfo = allKeys[appId + "/" + versionId] as CachedMetaData? else {
+        guard let allKeys = retrieveAllManifestInfo(), let manifestInfo = allKeys[cacheName], let key = manifestInfo.keys.first else {
             return nil
         }
-        return manifestInfo
-    }
-
-    /// Remove Key from the KeyChain
-    /// - Parameter keyId: Mini app ID
-    internal func removeKey(forMiniApp appId: String, versionId: String) {
-        var keysDic = retrieveAllManifestInfo()
-
-        keysDic?[appId + "/" + versionId] = nil
-
-        if let keys = keysDic {
-            keychainStore.setInfoInKeyChain(keys: keys)
+        if key == (appId + "/" + versionId) {
+            return manifestInfo.values.first
+        } else {
+            return nil
         }
     }
 
