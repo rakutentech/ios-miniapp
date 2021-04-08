372a81f9e4b0aa619071bf3d383116d33a82dfd5
PM Villa <pierremichel.villa@rakuten.com>
2021-04-08 15:27:07 +0900

    update comments

--- MiniApp/Classes/core/JavascriptBridge/Chat/ChatMessageBridgeDelegate.swift
+++ MiniApp/Classes/core/JavascriptBridge/Chat/ChatMessageBridgeDelegate.swift
@@ -11,7 +11,8 @@ public protocol ChatMessageBridgeDelegate: class {
 
     /// Triggered when Mini App wants to send a message to a specific contact.
     /// Should send a message to the specified contactId without any prompt to the User.
-    /// Should invoke completionHandler success after message was successfully sent.
+    /// Should invoke completionHandler success with the ID of the contact which was sent the message.
+    /// If the user cancelled sending the message, should invoke completionHandler success with nil value.
     /// Should invoke completionHandler error when there was an error.
     func sendMessageToContact(_ contactId: String, message: MessageToContact, completionHandler: @escaping (Result<String?, MASDKError>) -> Void)
 
@@ -19,7 +20,7 @@ public protocol ChatMessageBridgeDelegate: class {
     /// Should open a contact chooser which allows the user to choose multiple contacts,
     /// and should then send the message to all chosen contacts.
     /// Should invoke completionHandler success with a list of IDs of the contacts which were successfully sent the message.
-    /// If the user cancelled sending the message, should invoked completionHandler success with an empty list.
+    /// If the user cancelled sending the message, should invoked completionHandler success with nil value.
     /// Should invoke completionHandler error when there was an error.
     func sendMessageToMultipleContacts(_ message: MessageToContact, completionHandler: @escaping (Result<[String]?, MASDKError>) -> Void)
 }
