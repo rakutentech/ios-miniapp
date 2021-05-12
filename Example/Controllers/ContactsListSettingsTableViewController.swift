ebcd0d6eb30efb429398e536d1e93738c5b35f35
Leo Joseph <58416454+rleojoseph@users.noreply.github.com>
2021-05-12 12:47:58 +0900

    Sample app/mini 2916 (#267)
    * Updated Send Message Permission enum
    
    * Updated contacts list with custom name and email
    
    * Edit Contact details
    
    * UI refresh
    
    Co-authored-by: PM Villa <pierremichel.villa@rakuten.com>

--- Example/Controllers/ContactsListSettingsTableViewController.swift
+++ Example/Controllers/ContactsListSettingsTableViewController.swift
@@ -22,12 +22,15 @@ class ContactsListSettingsTableViewController: UITableViewController {
         userContactList = getContactList()
         if userContactList?.count ?? 0 == 0 {
             userContactList = []
-            for autogen in 0...9 {
+            repeat {
+                let name = Self.randomFakeName()
                 userContactList?.append(
                     MAContact(id: UUID().uuidString,
-                              name: self.randomFakeName(),
-                              email: "name.\(autogen)@example.com"))
-            }
+                              name: name,
+                              email: Self.fakeMail(with: name)
+                    )
+                )
+            } while (userContactList?.count ?? 10) < 10
             updateContactList(list: self.userContactList)
         }
     }
@@ -36,8 +39,18 @@ class ContactsListSettingsTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell? ?? UITableViewCell()
         if let userContactList = userContactList, userContactList.indices.contains(indexPath.row) {
             let contact = userContactList[indexPath.row]
-            cell.detailTextLabel?.text = "id: \(contact.id)"
-            cell.textLabel?.text = contact.name
+            cell.detailTextLabel?.numberOfLines = 3
+            let titleSize = cell.textLabel?.font.pointSize ?? 12
+            var names = contact.name?.components(separatedBy: " ")
+            cell.textLabel?.attributedText = NSMutableAttributedString()
+                .normal(names?.removeFirst() ?? "", fontSize: titleSize)
+                .bold(" " + (names?.joined() ?? ""), fontSize: titleSize)
+            let size = cell.detailTextLabel?.font.pointSize ?? 10
+            cell.detailTextLabel?.attributedText = NSMutableAttributedString()
+                .bold("\(NSLocalizedString("contact.id", comment: "")): ", fontSize: size)
+                .normal(contact.id, fontSize: size-3)
+                .bold("\n\(NSLocalizedString("contact.email", comment: "")): ", fontSize: size)
+                .normal(contact.email ?? "", fontSize: size)
             if contactDelegate != nil {
                 if allowMultipleSelection {
                     cell.accessoryType = selectedContacts.contains(contact) ? .checkmark : .none
@@ -50,22 +63,33 @@ class ContactsListSettingsTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if let contact = userContactList?[indexPath.row] {
-
-            if allowMultipleSelection {
-                if selectedContacts.contains(contact) {
-                    selectedContacts.removeAll { contactToRemove -> Bool in
-                        contactToRemove == contact
-                    }
-                } else {
-                    selectedContacts.append(contact)
-                }
-                tableView.reloadRows(at: [indexPath], with: .automatic)
-            } else {
-                tableView.deselectRow(at: indexPath, animated: true)
-                selectedContacts = [contact]
+        if contactDelegate == nil {
+            if let contact = userContactList?[indexPath.row] {
+                editContact(title: "Edit Contact info",
+                            index: indexPath.row,
+                            contactId: contact.id,
+                            contactName: contact.name,
+                            contactEmail: contact.email)
             }
-            contactDelegate?.contactsController(self, didSelect: selectedContacts)
+            tableView.deselectRow(at: indexPath, animated: true)
+        } else {
+            if let contact = userContactList?[indexPath.row] {
+
+                if allowMultipleSelection {
+                    if selectedContacts.contains(contact) {
+                        selectedContacts.removeAll { contactToRemove -> Bool in
+                            contactToRemove == contact
+                        }
+                    } else {
+                        selectedContacts.append(contact)
+                    }
+                    tableView.reloadRows(at: [indexPath], with: .automatic)
+                } else {
+                    tableView.deselectRow(at: indexPath, animated: true)
+                    selectedContacts = [contact]
+                }
+                contactDelegate?.contactsController(self, didSelect: selectedContacts)
+            }
         }
     }
 
@@ -97,17 +121,16 @@ class ContactsListSettingsTableViewController: UITableViewController {
     }
 
     @IBAction func addContact() {
-        self.addCustomContactId(title: "Please enter the custom ID you would like to add in Contacts", message: "")
+        self.addCustomContactId(title: "Please enter custom contact details", message: "")
     }
 
     func addCustomContactId(title: String, message: String) {
         DispatchQueue.main.async {
-            self.getInputFromAlertWithTextField(title: title, message: message, textFieldDefaultValue: UUID().uuidString) { (_, textField) in
+            self.getInputFromAlertWithTextField(title: title, message: message, textFieldDefaultValue: UUID().uuidString) { (_, textField, name, email) in
                 if let textField = textField, let contactId = textField.text, contactId.count > 0, !contactId.trimTrailingWhitespaces().isEmpty {
-                    let contactListCount = self.userContactList?.count ?? 0
                     self.userContactList?.append(MAContact(id: contactId,
-                                                           name: self.randomFakeName(),
-                                                           email: "name.\(contactListCount)@example.com"))
+                                                           name: name,
+                                                           email: email))
                     updateContactList(list: self.userContactList)
                     self.tableView.reloadData()
                 } else {
@@ -117,24 +140,31 @@ class ContactsListSettingsTableViewController: UITableViewController {
         }
     }
 
-    func getInputFromAlertWithTextField(title: String? = nil, message: String? = nil, keyboardType: UIKeyboardType? = .asciiCapable, textFieldDefaultValue: String?, handler: ((UIAlertAction, UITextField?) -> Void)? = nil) {
+    func getInputFromAlertWithTextField(title: String? = nil,
+                                        message: String? = nil,
+                                        keyboardType: UIKeyboardType? = .asciiCapable,
+                                        textFieldDefaultValue: String?,
+                                        name: String? = randomFakeName(),
+                                        email: String? = nil,
+                                        handler: ((UIAlertAction, UITextField?, String, String) -> Void)? = nil) {
         DispatchQueue.main.asyncAfter(deadline: .now()) {
             let alert = UIAlertController(title: title,
                 message: message,
                 preferredStyle: .alert)
             var textObserver: NSObjectProtocol?
 
+            let email = email ?? Self.fakeMail(with: name)
+
             let okAction = UIAlertAction(title: MASDKLocale.localize(.ok), style: .default) { (action) in
                 if !alert.textFields![0].text!.isEmpty {
-                    handler?(action, alert.textFields?.first)
+                    handler?(action, alert.textFields?.first, alert.textFields?[1].text ?? "", alert.textFields?[2].text ?? "")
                 } else {
-                    handler?(action, nil)
+                    handler?(action, nil, "", "")
                 }
                 if let observer = textObserver {
                     NotificationCenter.default.removeObserver(observer)
                 }
             }
-
             alert.addTextField { (textField) in
                 textObserver = NotificationCenter.default.addObserver(forName: UITextField.textDidChangeNotification, object: textField, queue: OperationQueue.main) {_ in
                     okAction.isEnabled = !(textField.text?.isEmpty ?? true)
@@ -143,9 +173,20 @@ class ContactsListSettingsTableViewController: UITableViewController {
                 if let type = keyboardType {
                     textField.keyboardType = type
                 }
+                textField.placeholder = NSLocalizedString("contact.id", comment: "")
                 textField.clearButtonMode = .whileEditing
             }
 
+            alert.addTextField { (textField) in
+                textField.text = name
+                textField.placeholder = NSLocalizedString("contact.name", comment: "")
+                textField.clearButtonMode = .whileEditing
+            }
+            alert.addTextField { (textField) in
+                textField.text = email
+                textField.placeholder = NSLocalizedString("contact.email", comment: "")
+                textField.clearButtonMode = .whileEditing
+            }
             okAction.isEnabled = !(textFieldDefaultValue?.isEmpty ?? true)
             alert.addAction(UIAlertAction(title: MASDKLocale.localize(.cancel), style: .cancel, handler: nil))
             alert.addAction(okAction)
@@ -153,16 +194,38 @@ class ContactsListSettingsTableViewController: UITableViewController {
         }
     }
 
-    func randomFakeName() -> String {
+    func editContact(title: String, index: Int, contactId: String? = "", contactName: String? = "", contactEmail: String? = "") {
+        DispatchQueue.main.async {
+            self.getInputFromAlertWithTextField(title: title, message: "", textFieldDefaultValue: contactId, name: contactName, email: contactEmail) { (_, textField, name, email) in
+                if let textField = textField, let contactId = textField.text, contactId.count > 0, !contactId.trimTrailingWhitespaces().isEmpty {
+                    self.userContactList?[index] = MAContact(id: contactId, name: name, email: email)
+                    updateContactList(list: self.userContactList)
+                    self.tableView.reloadData()
+                } else {
+                    self.editContact(title: "Invalid Contact ID, please try again",
+                                     index: index,
+                                     contactId: contactId,
+                                     contactName: name,
+                                     contactEmail: email)
+                }
+            }
+        }
+    }
+
+    class func fakeMail(with name: String?) -> String {
+        name != nil ? name!.replacingOccurrences(of: " ", with: ".", options: .literal, range: nil) + "@example.com" : ""
+    }
+
+    class func randomFakeName() -> String {
         randomFakeFirstName() + " " + randomFakeLastName()
     }
 
-    func randomFakeFirstName() -> String {
+    class func randomFakeFirstName() -> String {
         let firstNameList = ["哲也", "太郎", "ピエール", "レオ", "Yvonne", "Jamie", "Leticia", "Priscilla", "Sidney", "Nancy", "Edmund", "Bill", "Megan"]
         return firstNameList.randomElement()!
     }
 
-    func randomFakeLastName() -> String {
+    class func randomFakeLastName() -> String {
         let lastNameList = ["古室", "楽天", "ビラ", "ジョゼフ", "Andrews", "Casey", "Gross", "Lane", "Thomas", "Patrick", "Strickland", "Nicolas", "Freeman"]
         return lastNameList.randomElement()!
     }
