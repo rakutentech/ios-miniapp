ec292932e92245a3fcbf2c46dcfc75e47013afff
Timotheus Laubengaier <timotheus@laubengaier.com>
2021-06-14 17:43:35 +0900

    replace miniapp.create displayment with miniappui launch

--- Example/Controllers/ViewController.swift
+++ Example/Controllers/ViewController.swift
@@ -166,7 +166,7 @@ class ViewController: UIViewController {
     func displayMiniApp(miniAppInfo: MiniAppInfo) {
         self.showProgressIndicator {
             self.currentMiniAppInfo = miniAppInfo
-            self.fetchMiniApp(for: miniAppInfo)
+            self.showMiniApp(for: miniAppInfo)
             self.currentMiniAppTitle = miniAppInfo.displayName
         }
     }
@@ -233,7 +233,7 @@ extension ViewController: MALaunchScreenDelegate {
                 return
             }
             self.showProgressIndicator {
-                self.fetchMiniApp(for: info)
+                self.showMiniApp(for: info)
                 self.currentMiniAppTitle = info.displayName
             }
         }
