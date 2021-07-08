6afaf16f04d2b3c2093e9d058e17d593fcaf2bf8
Leo Joseph <leojoseph.r@rakuten.com>
2021-07-09 02:05:54 +0900

    Updated testcases

--- Tests/Unit/MiniAppTests.swift
+++ Tests/Unit/MiniAppTests.swift
@@ -2,6 +2,7 @@ import Quick
 import Nimble
 @testable import MiniApp
 
+// swiftlint:disable function_body_length
 class MiniAppTests: QuickSpec {
 
     override func spec() {
@@ -55,6 +56,49 @@ class MiniAppTests: QuickSpec {
                         }
                     })
                     expect(testError?.localizedDescription).to(equal(MASDKError.invalidVersionId.localizedDescription))
+                }
+            }
+            context("when createMiniapp is called with invalid miniapp appId") {
+                it("will return invalidAppId error") {
+                    var testError: MASDKError?
+                    let mockMessageInterface = MockMessageInterface()
+                    MiniApp.shared().create(appId: "", completionHandler: { (result) in
+                        switch result {
+                        case .success: break
+                        case .failure(let error):
+                            testError = error
+                        }
+                    }, messageInterface: mockMessageInterface)
+                    expect(testError?.localizedDescription).toEventually(equal(MASDKError.invalidAppId.localizedDescription), timeout: .seconds(5))
+                }
+            }
+            context("when createMiniapp is called with invalid miniapp version") {
+                it("will return invalidAppId error") {
+                    var testError: MASDKError?
+                    let mockMessageInterface = MockMessageInterface()
+                    MiniApp.shared().create(appId: "1", version: "", queryParams: nil, completionHandler: { (result) in
+                        switch result {
+                        case .success: break
+                        case .failure(let error):
+                            testError = error
+                            print("")
+                        }
+                    }, messageInterface: mockMessageInterface, adsDisplayer: nil)
+                    expect(testError?.localizedDescription).toEventuallyNot(beNil(), timeout: .seconds(5))
+                }
+            }
+            context("when createMiniapp is called with invalid miniapp version") {
+                it("will return invalidAppId error") {
+                    var testError: MASDKError?
+                    let mockMessageInterface = MockMessageInterface()
+                    MiniApp.shared().create(appId: "1", version: "1", queryParams: nil, completionHandler: { (result) in
+                        switch result {
+                        case .success: break
+                        case .failure(let error):
+                            testError = error
+                        }
+                    }, messageInterface: mockMessageInterface, adsDisplayer: nil)
+                    expect(testError?.localizedDescription).toEventually(contain("Server returned an error"), timeout: .seconds(5))
                 }
             }
         }
