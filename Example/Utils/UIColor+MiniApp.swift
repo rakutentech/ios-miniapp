eabe9d51574da295ed3d8b43bf8874b039f95973
PM Villa <pierremichel.villa@rakuten.com>
2021-07-12 13:41:46 +0900

    removed bloat comments

--- Example/Utils/UIColor+MiniApp.swift
+++ Example/Utils/UIColor+MiniApp.swift
@@ -1,11 +1,3 @@
-//
-//  UIColor+MiniApp.swift
-//  MiniApp_Example
-//
-//  Created by Villa, Pierre | Pm | MTSD on 2021/07/09.
-//  Copyright © 2021 Rakuten Group, Inc. All rights reserved.
-//
-
 import UIKit
 
 extension UIColor {
