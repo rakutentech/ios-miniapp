875e6f26935f300dbef61a04a2e660d7694480a3
Leo Joseph <leojoseph.r@rakuten.com>
2021-07-21 11:50:45 +0900

    Refactor

--- Example/Controllers/Deeplink/DeeplinkErrorViewController.swift
+++ Example/Controllers/Deeplink/DeeplinkErrorViewController.swift
@@ -4,27 +4,42 @@ import MiniApp
 class DeeplinkErrorViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var errorDescTextview: UITextView!
     @IBOutlet weak var errorTitleLabel: UILabel!
-    var errorType: DeeplinkErrorDescriptionType? = .noLongerExists
+    var errorType: DeeplinkErrorDescriptionType? = .miniAppNoLongerExists
     var errorTitle: DeeplinkErrorTitleType?
+    var miniAppInfo: MiniAppInfo?
 
     override func viewWillAppear(_ animated: Bool) {
         errorDescTextview.createHyperLinkAttributedText(
             fullText: MASDKLocale.localize(errorType?.rawValue ?? "deeplink.ui.miniapp.error.desc"),
             textToLink: MASDKLocale.localize("deeplink.ui.miniapp.error.help.center"),
             urlString: MASDKLocale.localize("deeplink.ui.miniapp.error.help.center.hyperlink"))
-        guard let title = errorTitle else { return }
-        errorTitleLabel.text = String(format: MASDKLocale.localize(title.rawValue))
+        setErrorTitle(title: errorTitle)
     }
+
     @IBAction func close(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+
+    func setErrorTitle(title: DeeplinkErrorTitleType? = DeeplinkErrorTitleType.qrCodeExpiredTitle) {
+        guard let title = errorTitle else { return }
+        switch title {
+        case .versionNotValidTitle:
+            errorTitleLabel.text = String(format: MASDKLocale.localize(title.rawValue), miniAppInfo?.version.versionTag ?? "")
+        case .cannotBePreviewedTitle:
+            errorTitleLabel.text = String(format: MASDKLocale.localize(title.rawValue), miniAppInfo?.displayName ?? "MiniApp", miniAppInfo?.version.versionTag ?? "")
+        default:
+            errorTitleLabel.text = String(format: MASDKLocale.localize(title.rawValue))
+        }
     }
 }
 
 enum DeeplinkErrorDescriptionType: String {
-    case noLongerExists = "deeplink.ui.miniapp.error.desc"
-    case permissionError = "deeplink.ui.miniapp.permissionerror.desc"
+    case miniAppNoLongerExists = "deeplink.ui.miniapp.error.desc"
+    case miniAppPermissionError = "deeplink.ui.miniapp.permissionerror.desc"
+
     case qrCodeExpired = "deeplink.ui.qr.error.desc"
-    case cannotBePreviewed, versionNotValid = "deeplink.ui.version.notvalid.error.desc"
+    case cannotBePreviewed = "deeplink.ui.version.cannotBePreviewed.error.desc"
+    case versionNotValid = "deeplink.ui.version.notvalid.error.desc"
 }
 
 enum DeeplinkErrorTitleType: String {
