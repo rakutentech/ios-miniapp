bea8de89317570b9ff4ba916271983a43c60adb6
Leo Joseph <58416454+rleojoseph@users.noreply.github.com>
2021-10-20 13:57:15 +0900

    [MINI-3964] Added Copy to clipboard feature (#162)
    * Added Copy to clipboard feature
    
    * Refactor
    
    * Refactor
    
    * Refactor

--- js-miniapp-sample/src/pages/uuid-sdk.js
+++ js-miniapp-sample/src/pages/uuid-sdk.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   Button,
   CardContent,
   CardActions,
   makeStyles,
+  Snackbar,
 } from '@material-ui/core';
 import { connect } from 'react-redux';
 
@@ -39,6 +40,47 @@ type UUIDProps = {
 
 const UuidFetcher = (props: UUIDProps) => {
   const classes = useStyles();
+  const [copyStatus, setCopyStatus] = useState({
+    success: false,
+    error: false,
+  });
+
+  function copyToClipboard() {
+    if (props.uuid === undefined) {
+      return;
+    }
+    if (!navigator.clipboard) {
+      fallbackCopyMethod(props.uuid);
+      return;
+    }
+    navigator.clipboard.writeText(props.uuid).then(
+      function () {
+        setCopyStatus({ success: true, error: false });
+      },
+      function (err) {
+        setCopyStatus({ success: false, error: true });
+      }
+    );
+  }
+
+  function fallbackCopyMethod(text) {
+    var textArea = document.createElement('textarea');
+    textArea.value = text;
+    textArea.style.top = '0';
+    textArea.style.left = '0';
+    textArea.style.position = 'fixed';
+    document.body !== null && document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    try {
+      document.execCommand('copy');
+      setCopyStatus({ success: true, error: false });
+    } catch (err) {
+      setCopyStatus({ success: false, error: true });
+    }
+    document.body !== null && document.body.removeChild(textArea);
+  }
+
   return (
     <GreyCard>
       <CardContent className={classes.content}>
@@ -54,6 +96,31 @@ const UuidFetcher = (props: UUIDProps) => {
         >
           GET UNIQUE ID
         </Button>
+        <Button
+          disabled={!props.uuid}
+          data-testid="clipboard-copy"
+          variant="contained"
+          color="primary"
+          onClick={copyToClipboard}
+        >
+          Copy
+        </Button>
+        <Snackbar
+          open={copyStatus.success}
+          autoHideDuration={3000}
+          onClose={() => {
+            setCopyStatus({ success: false, error: false });
+          }}
+          message="Unique ID copied !!"
+        />
+        <Snackbar
+          open={copyStatus.error}
+          autoHideDuration={3000}
+          onClose={() => {
+            setCopyStatus({ success: false, error: false });
+          }}
+          message="Failed to copy!"
+        />
       </CardActions>
     </GreyCard>
   );
