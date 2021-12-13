0ee7a36e1589ca3f618d8eb6cb874d09035e7c42
PM Villa <pierremichel.villa@rakuten.com>
2021-12-14 02:53:49 +0900

    SPM checksum

--- Package.swift
+++ Package.swift
@@ -13,14 +13,15 @@ let package = Package(
             targets: ["MiniApp"])
     ],
     dependencies: [
-        .package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMajor(from: "0.9")),
-        .package(url: "https://github.com/datatheorem/TrustKit"),
-        .package(url: "https://github.com/Climbatize/GoogleMobileAds.git" , .upToNextMajor(from: "8.13.0"))
+        .package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMajor(from: "0.9.0")),
+        .package(url: "https://github.com/datatheorem/TrustKit", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/Climbatize/GoogleMobileAds.git" , .upToNextMajor(from: "8.12.0"))
     ],
     targets: [
         .binaryTarget(
             name: "MiniApp",
-            url: "https://github.com/rakutentech/ios-miniapp/releases/download/v3.9.0/MiniApp.xcframework.zip"
+            url: "https://github.com/rakutentech/ios-miniapp/releases/download/v3.9.0/MiniApp.xcframework.zip",
+            checksum: "8764c367820a108ef73097cb7125b931ab7026f6b536cfa81db26f8cd3a81703"
         )
     ],
     swiftLanguageVersions: [
