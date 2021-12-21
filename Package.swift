3650dcc6873464570f0a6dd2da03912688cee7d5
Pierre-Michel Villa <pierremichel.villa@rakuten.com>
2021-12-21 12:27:25 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MiniApp",
-            url: "https://github.com/rakutentech/ios-miniapp/releases/download/3.9.0/MiniApp.xcframework.zip",
+            url: "https://github.com/rakutentech/ios-miniapp/releases/download/v3.9.0/MiniApp.xcframework.zip",
             checksum: "fe56fa64bb053ada7243f8cb232ed0f8ef859a02bcb9930e2d74112cf0a3f9d2"
         )
     ],
