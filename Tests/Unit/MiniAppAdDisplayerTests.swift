c5a1d69228ff9774ae48cdccc5e913e09c2317ba
Leo Joseph <58416454+rleojoseph@users.noreply.github.com>
2022-02-08 18:34:12 +0900

    Initial Commit (#26)
    * Initial Commit
    
    * Code Cov refactor
    
    * Added ignore list in slather
    
    * Added Ignore list in slather
    
    * Created Slather.yml

--- Tests/Unit/MiniAppAdDisplayerTests.swift
+++ Tests/Unit/MiniAppAdDisplayerTests.swift
@@ -99,6 +99,45 @@ class MiniAppAdDisplayerTests: QuickSpec {
                 }
             }
         }
+        describe("Mini app ad display error") {
+            context("when MASDKAdsDisplayError is returned") {
+                it("will return UKNOWN_ERROR error, description and description is not empty") {
+                    let error = MASDKAdsDisplayError(rawValue: "UKNOWN_ERROR")
+                    expect(error).toEventually(equal(.unknownError))
+                    expect(error?.description).notTo(beEmpty())
+                }
+                it("will return error, description and description is not empty") {
+                    let error = MASDKAdsDisplayError(rawValue: "FAILED_TO_CONFORM_PROTOCOL")
+                    expect(error).toEventually(equal(.failedToConformToProtocol))
+                    expect(error?.description).notTo(beEmpty())
+                }
+                it("will return error, description and description is not empty") {
+                    let error = MASDKAdsDisplayError(rawValue: "sdkError")
+                    expect(error).toEventually(equal(.sdkError))
+                    expect(error?.description).notTo(beEmpty())
+                }
+                it("will return error, description and description is not empty") {
+                    let error = MASDKAdsDisplayError(rawValue: "adNotLoaded")
+                    expect(error).toEventually(equal(.adNotLoaded))
+                    expect(error?.description).notTo(beEmpty())
+                }
+                it("will return error, description and description is not empty") {
+                    let error = MASDKAdsDisplayError(rawValue: "rewardFailure")
+                    expect(error).toEventually(equal(.rewardFailure))
+                    expect(error?.description).notTo(beEmpty())
+                }
+                it("will return error, description and description is not empty") {
+                    let error = MASDKAdsDisplayError(rawValue: "hostUIError")
+                    expect(error).toEventually(equal(.hostUIError))
+                    expect(error?.description).notTo(beEmpty())
+                }
+                it("will return error, description and description is not empty") {
+                    let error = MASDKAdsDisplayError(rawValue: "adIdError")
+                    expect(error).toEventually(equal(.adIdError))
+                    expect(error?.description).notTo(beEmpty())
+                }
+            }
+        }
     }
 }
 
