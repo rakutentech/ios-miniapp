bf9c3084871b8f50429f0bb2f6187cd25c7b0195
Leo Joseph <leojoseph.r@rakuten.com>
2022-03-01 16:18:11 +0900

    SPM checksum

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .binaryTarget(
             name: "MiniApp",
             url: "https://github.com/rakutentech/ios-miniapp/releases/download/v4.0.0/MiniApp.xcframework.zip",
-            checksum: "0a7df49ae845acc64ebf4e490aab0eea29294c84f4fa4ba47f76b905081ae7b2"
+            checksum: "51a8f14ba14cedf67c31f62ff22e3e954a506de6f098eb683c1e505179f8024a"
             dependencies: ["ZIPFoundation", "TrustKit", "GoogleMobileAds-SPM"]
         )
     ],
