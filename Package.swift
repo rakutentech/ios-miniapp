fedb114c2d68b71d001b9f3bef04dd1682c8de76
Timotheus Laubengaier (M1) <timotheus@laubengaier.com>
2022-03-17 15:08:56 +0900

    test

--- Package.swift
+++ Package.swift
@@ -18,11 +18,12 @@ let package = Package(
         .package(url: "https://github.com/rakutentech/GoogleMobileAds-SPM.git", .upToNextMajor(from: "8.13.0"))
     ],
     targets: [
-        .binaryTarget(
-            name: "MiniApp",
-            url: "https://github.com/rakutentech/ios-miniapp/releases/download/v4.0.0/MiniApp.xcframework.zip",
-            checksum: "dbde912b6717dce3db5f7c5f3e1ecbc37cc9b54cede575fbad2f259b2c72b3be"
-        )
+        .target(name: "MiniApp", dependencies: ["ZIPFoundation", "TrustKit", "GoogleMobileAds-SPM.git"])
+//        .binaryTarget(
+//            name: "MiniApp",
+//            url: "https://github.com/rakutentech/ios-miniapp/releases/download/v4.0.0/MiniApp.xcframework.zip",
+//            checksum: "dbde912b6717dce3db5f7c5f3e1ecbc37cc9b54cede575fbad2f259b2c72b3be"
+//        )
     ],
     swiftLanguageVersions: [
         .v5
