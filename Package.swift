2347033176e5c9c76565b7b2eeb00580ceb18803
Timotheus Laubengaier (M1) <timotheus@laubengaier.com>
2022-03-23 16:35:48 +0900

    test

--- Package.swift
+++ Package.swift
@@ -28,11 +28,10 @@ let package = Package(
             exclude: [
                 "core/Utilities/LoaderObjC.m",
                 "admob7/AdMobDisplayer.swift"
+            ],
+            resources: [
+                .process("Resources")
             ]
-//            resources: [
-//                .process("en.lproj"),
-//                .process("Resources")
-//            ]
         ),
         // issues
         // swift toolkit version (5.4.2 <> 5.5.2)
