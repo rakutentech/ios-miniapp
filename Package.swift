54aa0609265a3613125e87b549cae2af28d4939e
Timotheus Laubengaier (M1) <timotheus@laubengaier.com>
2022-03-23 14:38:12 +0900

    fix

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
-        .library(name: "MiniApp", targets: ["MiniApp-Static"]),
-        .library(name: "MiniApp-Binary", targets: ["MiniApp"])
+        .library(name: "MiniApp", targets: ["MiniApp"])
+        //.library(name: "MiniApp-Binary", targets: ["MiniApp"])
     ],
     dependencies: [
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMajor(from: "0.9.0")),
@@ -33,11 +33,11 @@ let package = Package(
         // issues
         // swift toolkit version (5.4.2 <> 5.5.2)
         // no such module GoogleMobileAds
-        .binaryTarget(
-            name: "MiniApp",
-            url: "https://github.com/rakutentech/ios-miniapp/releases/download/v4.0.0/MiniApp.xcframework.zip",
-            checksum: "dbde912b6717dce3db5f7c5f3e1ecbc37cc9b54cede575fbad2f259b2c72b3be"
-        )
+        //.binaryTarget(
+        //    name: "MiniApp",
+        //    url: "https://github.com/rakutentech/ios-miniapp/releases/download/v4.0.0/MiniApp.xcframework.zip",
+        //    checksum: "dbde912b6717dce3db5f7c5f3e1ecbc37cc9b54cede575fbad2f259b2c72b3be"
+        //)
     ],
     swiftLanguageVersions: [
         .v5
