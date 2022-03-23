923d6bf754370fd2cc5fd1fe96131bb81ce97874
Timotheus Laubengaier (M1) <timotheus@laubengaier.com>
2022-03-23 14:43:26 +0900

    fix

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "MiniApp-Static",
+            name: "MiniApp",
             dependencies: [
                 "ZIPFoundation",
                 "TrustKit",
