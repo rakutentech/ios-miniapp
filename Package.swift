e6b2a91130d298d9f7d879a19556f89bb035fb48
Timotheus Laubengaier (M1) <timotheus@laubengaier.com>
2022-03-23 12:50:32 +0900

    naming test

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMajor(from: "0.9.0")),
         .package(url: "https://github.com/datatheorem/TrustKit.git", .upToNextMajor(from: "2.0.0")),
-        .package(url: "https://github.com/rakutentech/GoogleMobileAds-SPM.git", .upToNextMajor(from: "9.0.0"))
+        .package(name: "GoogleMobileAds-SPM", url: "https://github.com/rakutentech/GoogleMobileAds-SPM.git", .upToNextMajor(from: "9.0.0"))
     ],
     targets: [
         .target(
@@ -30,16 +30,14 @@ let package = Package(
                 "admob7/AdMobDisplayer.swift"
             ]
         ),
+        // issues
+        // swift toolkit version (5.4.2 <> 5.5.2)
+        // no such module GoogleMobileAds
         .binaryTarget(
             name: "MiniApp",
             url: "https://github.com/rakutentech/ios-miniapp/releases/download/v4.0.0/MiniApp.xcframework.zip",
             checksum: "dbde912b6717dce3db5f7c5f3e1ecbc37cc9b54cede575fbad2f259b2c72b3be"
         )
-        // for local testing
-//        .binaryTarget(
-//            name: "MiniApp-Local",
-//            path: "Binary/XCFramework/MiniApp.xcframework"
-//        )
     ],
     swiftLanguageVersions: [
         .v5
