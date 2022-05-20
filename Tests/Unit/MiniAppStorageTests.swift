eab9162c7b0521ab80f6805ce864e7b9d98d016e
Timotheus Laubengaier (M1) <timotheus@laubengaier.com>
2022-05-20 15:37:50 +0900

    fix

--- Tests/Unit/MiniAppStorageTests.swift
+++ Tests/Unit/MiniAppStorageTests.swift
@@ -2,7 +2,7 @@ import Quick
 import Nimble
 @testable import MiniApp
 
-// swiftlint:disable function_body_length
+// swiftlint:disable function_body_length cyclomatic_complexity
 class MiniAppStorageTests: QuickSpec {
 
     override func spec() {
