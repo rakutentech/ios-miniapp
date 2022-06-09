6d433643572f86bcd2c580ae845557353140c28a
Timotheus Laubengaier (M1) <timotheus@laubengaier.com>
2022-06-09 13:55:30 +0900

    bugfix for tap on close

--- Example/Controllers/Extensions/ViewController+Activity.swift
+++ Example/Controllers/Extensions/ViewController+Activity.swift
@@ -2,5 +2,16 @@ import Foundation
 import UIKit
 
 public class MiniAppActivityController: UIActivityViewController {
-    public override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {}
+    public override init(activityItems: [Any], applicationActivities: [UIActivity]?) {
+        super.init(activityItems: activityItems, applicationActivities: applicationActivities)
+        completionWithItemsHandler = { (activityType, completed: Bool, returnedItems:[Any]?, error: Error?) in
+            if !completed {
+                super.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+
+    public override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
+        completion?()
+    }
 }
