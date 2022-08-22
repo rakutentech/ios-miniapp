fc7dbf0c154505349163453279e59c2bfd994cac
Timotheus Laubengaier (M1) <timotheus@laubengaier.com>
2022-08-22 15:35:49 +0900

    lint

--- Example/Controllers/SwiftUI/Helper/RemoteImageView.swift
+++ Example/Controllers/SwiftUI/Helper/RemoteImageView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 
 class ImageLoaderService: ObservableObject {
     @Published var image: UIImage = UIImage()
-    
+
     func loadImage(for urlString: String) {
         guard let url = URL(string: urlString) else { return }
-        
+
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data else { return }
             DispatchQueue.main.async {
@@ -29,7 +29,7 @@ struct RemoteImageView: View {
     var urlString: String
     @ObservedObject var imageLoader = ImageLoaderService()
     @State var image: UIImage = UIImage()
-    
+
     var body: some View {
         Image(uiImage: image)
             .resizable()
