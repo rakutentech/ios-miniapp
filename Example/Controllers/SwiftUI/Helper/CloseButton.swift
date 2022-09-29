af2dbe3bc24335b7e66309ad9db9fa678014b465
Timotheus Laubengaier (M1) <timotheus@laubengaier.com>
2022-09-29 12:56:27 +0900

    adding close button to share preview

---
+++ Example/Controllers/SwiftUI/Helper/CloseButton.swift
@@ -0,0 +1,41 @@
+//
+//  CloseButton.swift
+//  Sample SPM
+//
+//  Created by Timotheus Laubengaier on 2022/09/29.
+//
+
+import SwiftUI
+
+struct CloseButton: View {
+
+    var action: () -> Void
+
+    init(action: @escaping () -> Void) {
+        self.action = action
+    }
+
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            ZStack {
+                Circle()
+                    .fill(Color(.secondarySystemBackground))
+                    .frame(width: 30, height: 30, alignment: .center)
+                Image(systemName: "xmark")
+                    .font(.system(size: 12, weight: .bold, design: .rounded))
+                    .foregroundColor(.secondary)
+            }
+            .contentShape(Circle())
+        }
+    }
+}
+
+struct CloseButton_Previews: PreviewProvider {
+    static var previews: some View {
+        CloseButton {
+            print("pressed button")
+        }
+    }
+}
