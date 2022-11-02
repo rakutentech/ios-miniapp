e40fb15dd19b1cd0f4ae3b09f9cdd2f17c55bc18
Timotheus Laubengaier <timotheus@laubengaier.com>
2022-11-02 16:34:29 +0900

    implement search

--- Example/Controllers/SwiftUI/Features/List/MiniAppListView.swift
+++ Example/Controllers/SwiftUI/Features/List/MiniAppListView.swift
@@ -39,30 +39,7 @@ struct MiniAppListView: View {
                     Text("No MiniApps found")
                         .foregroundColor(Color(.secondaryLabel))
                 } else {
-                    List {
-                        ForEach(viewModel.indexedMiniAppInfoList.keys.sorted(), id: \.self) { (key) in
-                            Section(header: Text(key)) {
-                                ForEach(viewModel.indexedMiniAppInfoList[key]!, id: \.version) { (info) in
-                                    NavigationLink {
-                                        MiniAppSingleView(
-                                            listType: viewModel.type,
-                                            miniAppId: info.id,
-                                            miniAppVersion: info.version.versionId,
-                                            miniAppType: .miniapp
-                                        )
-                                    } label: {
-                                        MiniAppListRowCell(
-                                            iconUrl: info.icon,
-                                            displayName: info.displayName ?? "",
-                                            versionTag: info.version.versionTag,
-                                            versionId: info.version.versionId
-                                        )
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    .listStyle(GroupedListStyle())
+                    MiniAppIndexedListView(viewModel: viewModel)
                 }
             }
         }
@@ -109,4 +86,46 @@ struct MiniAppListView_Previews: PreviewProvider {
 struct MiniAppSingleViewRequest: Identifiable {
     let id = UUID()
     let info: MiniAppInfo
+}
+
+// MARK: - MiniAppIndexedListView
+extension MiniAppListView {
+	struct MiniAppIndexedListView: View {
+
+		@ObservedObject var viewModel: MiniAppListViewModel
+		@State private var searchText: String = ""
+
+		var body: some View {
+			VStack {
+				TextField("Search... (id, version)", text: $viewModel.searchText)
+					.textFieldStyle(MiniAppSearchTextFieldStyle())
+					.padding(.vertical, 10)
+					.padding(.horizontal, 16)
+				List {
+					ForEach(viewModel.filteredIndexedMiniAppInfoList.keys.sorted(), id: \.self) { (key) in
+						Section(header: Text(key)) {
+							ForEach(viewModel.filteredIndexedMiniAppInfoList[key]!, id: \.version) { (info) in
+								NavigationLink {
+									MiniAppSingleView(
+										listType: viewModel.type,
+										miniAppId: info.id,
+										miniAppVersion: info.version.versionId,
+										miniAppType: .miniapp
+									)
+								} label: {
+									MiniAppListRowCell(
+										iconUrl: info.icon,
+										displayName: info.displayName ?? "",
+										versionTag: info.version.versionTag,
+										versionId: info.version.versionId
+									)
+								}
+							}
+						}
+					}
+				}
+				.listStyle(GroupedListStyle())
+			}
+		}
+	}
 }
--- Example/Controllers/SwiftUI/Features/List/MiniAppListViewModel.swift
+++ Example/Controllers/SwiftUI/Features/List/MiniAppListViewModel.swift
@@ -5,83 +5,141 @@ import Combine
 @MainActor
 class MiniAppListViewModel: ObservableObject {
 
-    let store = MiniAppStore.shared
+	let store = MiniAppStore.shared
 
-    @Published var type: MiniAppSettingsView.ListConfig
-    @Published var state: ListState = .none
-    @Published var indexedMiniAppInfoList: [String: [MiniAppInfo]] = [:]
+	@Published var type: MiniAppSettingsView.ListConfig
+	@Published var state: ListState = .none
+	@Published var indexedMiniAppInfoList: [String: [MiniAppInfo]] = [:]
 
-    var bag = Set<AnyCancellable>()
+	@Published var searchText: String = ""
+	@Published var filteredIndexedMiniAppInfoList: [String: [MiniAppInfo]] = [:]
 
-    init(type: MiniAppSettingsView.ListConfig) {
-        self.type = type
-        setupObservers()
-        if store.miniAppSetupCompleted {
-            load()
-        } else {
-            state = .awaitsSetup
-        }
-    }
+	var bag = Set<AnyCancellable>()
 
-    func setupObservers() {
-        switch type {
-        case .listI:
-            store
-                .$miniAppInfoList
-                .receive(on: DispatchQueue.main)
-                .sink { [weak self] list in
-                    guard let self = self, self.store.miniAppSetupCompleted else {
-                        self?.state = .awaitsSetup
-                        return
-                    }
-                    self.state = .success
-                    self.indexedMiniAppInfoList = self.makeIndexed(infos: list)
-                }
-                .store(in: &bag)
-        case .listII:
-            store
-                .$miniAppInfoList2
-                .receive(on: DispatchQueue.main)
-                .sink { [weak self] list in
-                    guard let self = self, self.store.miniAppSetupCompleted else {
-                        self?.state = .awaitsSetup
-                        return
-                    }
-                    self.state = .success
-                    self.indexedMiniAppInfoList = self.makeIndexed(infos: list)
-                }
-                .store(in: &bag)
-        }
-    }
+	init(type: MiniAppSettingsView.ListConfig) {
+		self.type = type
+		setupObservers()
+		if store.miniAppSetupCompleted {
+			load()
+		} else {
+			state = .awaitsSetup
+		}
+	}
 
-    func makeIndexed(infos: [MiniAppInfo]) -> [String: [MiniAppInfo]] {
-        let ids = Set<String>(infos.map({ $0.id }))
-        var newList: [String: [MiniAppInfo]] = [:]
-        for id in ids {
-            newList[id] = infos.filter({ $0.id == id })
-        }
-        return newList
-    }
+	func setupObservers() {
+		switch type {
+		case .listI:
+			store
+				.$miniAppInfoList
+				.receive(on: DispatchQueue.main)
+				.sink { [weak self] list in
+					guard let self = self, self.store.miniAppSetupCompleted else {
+						self?.state = .awaitsSetup
+						return
+					}
+					let indexedList = self.makeIndexed(infos: list)
+					self.indexedMiniAppInfoList = indexedList
+					self.filteredIndexedMiniAppInfoList = self.applyCurrentFilter(indexedList)
+					self.state = .success
+				}
+				.store(in: &bag)
+		case .listII:
+			store
+				.$miniAppInfoList2
+				.receive(on: DispatchQueue.main)
+				.sink { [weak self] list in
+					guard let self = self, self.store.miniAppSetupCompleted else {
+						self?.state = .awaitsSetup
+						return
+					}
+					let indexedList = self.makeIndexed(infos: list)
+					self.indexedMiniAppInfoList = self.makeIndexed(infos: list)
+					self.filteredIndexedMiniAppInfoList = self.applyCurrentFilter(indexedList)
+					self.state = .success
+				}
+				.store(in: &bag)
+		}
 
-    func checkSetup() {
-        if !store.miniAppSetupCompleted {
-            state = .awaitsSetup
-        }
-    }
+		$searchText
+			.receive(on: DispatchQueue.main)
+			.sink { [weak self] text in
+				guard let self = self else {
+					return
+				}
+				self.updateFilter(text: text)
+			}
+			.store(in: &bag)
+	}
 
-    private func load() {
-        state = .loading
-        store.load(type: type)
-    }
+	func makeIndexed(infos: [MiniAppInfo]) -> [String: [MiniAppInfo]] {
+		let ids = Set<String>(infos.map({ $0.id }))
+		var newList: [String: [MiniAppInfo]] = [:]
+		for id in ids {
+			newList[id] = infos.filter({ $0.id == id })
+		}
+		return newList
+	}
 
-    var config: MiniAppSdkConfig {
-        store.config.sdkConfig(list: type)
-    }
+	func checkSetup() {
+		if !store.miniAppSetupCompleted {
+			state = .awaitsSetup
+		}
+	}
+
+	private func load() {
+		state = .loading
+		store.load(type: type)
+	}
+
+	var config: MiniAppSdkConfig {
+		store.config.sdkConfig(list: type)
+	}
+
+	func applyCurrentFilter(_ targetList: [String: [MiniAppInfo]]) -> [String: [MiniAppInfo]] {
+		guard !searchText.isEmpty else {
+			return targetList
+		}
+		return targetList.filter({ $0.0.lowercased().contains(searchText) })
+	}
+
+	func updateFilter(text: String) {
+		let searchText = text.trimmingCharacters(in: .whitespacesAndNewlines)
+		guard !searchText.isEmpty else {
+			filteredIndexedMiniAppInfoList = indexedMiniAppInfoList
+			return
+		}
+
+		// deeplink search, not applicabale right now
+		// if searchText.starts(with: "miniappdemo://miniapp/preview/"), let searchTextUrl = URL(string: searchText) {
+		// 	let miniAppId = searchTextUrl.pathComponents[2]
+		// 	searchText = miniAppId
+		// }
+
+		if isUUID(searchText), let identifierFound = indexedMiniAppInfoList[searchText.uppercased()] {
+			filteredIndexedMiniAppInfoList = [searchText: identifierFound]
+			return
+		}
+
+		if isUUID(searchText), let versionFound = indexedMiniAppInfoList.map({ $0.1 }).reduce([], +).first(where: { $0.version.versionId == searchText }) {
+			filteredIndexedMiniAppInfoList = [versionFound.id: [versionFound]]
+			return
+		}
+
+		filteredIndexedMiniAppInfoList = Dictionary(uniqueKeysWithValues: indexedMiniAppInfoList.map({ (key, value) in
+			(key, value.filter({ $0.displayName?.lowercased().contains(searchText.lowercased()) ?? false }))
+		}))
+		.filter({ !$0.value.isEmpty })
+	}
+
+	func isUUID(_ text: String) -> Bool {
+		let uuidRegex = #"^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"#
+		return text.range(of: uuidRegex, options: .regularExpression) != nil
+	}
 }
 
 enum ListState {
-    case none
-    case awaitsSetup
-    case loading
-    case success
+	case none
+	case awaitsSetup
+	case loading
+	case success
 }
