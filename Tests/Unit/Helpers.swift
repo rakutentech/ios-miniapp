0683d59d77a593aadb3a69b4146ead539b3b9f42
GondiTilak <tilak.gondi@rakuten.com>
2022-12-28 14:13:08 +0900

    [MINI-5826] Update Demo app Settings to support multiple emails
    Updated test input as per suggestions
    
    **Link:**
    [MINI-5826](https://jira.rakuten-it.com/jira/browse/MINI-5826)
    [Demo Video on Box](https://rak.box.com/s/npe1sx6jjj2uicgcyc7yjv3tl6i0id6i)

--- Tests/Unit/Helpers.swift
+++ Tests/Unit/Helpers.swift
@@ -413,7 +413,7 @@ class MockMessageInterface: MiniAppMessageDelegate {
             id: "contact_id",
             name: MiniAppStore.randomFakeName(),
             email: MiniAppStore.fakeMail(with: MiniAppStore.randomFakeName()),
-            allEmailList: ["test@test.com"]
+            allEmailList: [MiniAppStore.fakeMail(with: MiniAppStore.randomFakeName()),MiniAppStore.fakeMail(with: MiniAppStore.randomFakeName()),MiniAppStore.fakeMail(with: MiniAppStore.randomFakeName())]
         )
     ]
     var messageContentAllowed: Bool = false
