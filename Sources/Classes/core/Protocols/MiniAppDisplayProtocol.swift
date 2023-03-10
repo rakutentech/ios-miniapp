2be6b67f7a4501784106ba880bb0d1b58194f4ec
Leo Joseph <leojoseph.r@rakuten.com>
2023-03-10 12:31:50 +0900

    Fix for sendJsonToMiniapp

--- Sources/Classes/core/Protocols/MiniAppDisplayProtocol.swift
+++ Sources/Classes/core/Protocols/MiniAppDisplayProtocol.swift
@@ -9,5 +9,8 @@ public protocol MiniAppDisplayDelegate: AnyObject {
 
     /// Get the view of the Mini app
     func getMiniAppView() -> UIView
+    
+    /// Interface  that will be used by the Host app to send Json
+    func sendJsonToMiniApp(string jsonString: String)
 
 }
