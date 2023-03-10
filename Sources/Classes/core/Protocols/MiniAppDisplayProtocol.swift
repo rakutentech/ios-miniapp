65fc023e25179e4e61e5952697f011bee69dea03
Leo Joseph <leojoseph.r@rakuten.com>
2023-03-10 13:21:00 +0900

    Swiftlint fix

--- Sources/Classes/core/Protocols/MiniAppDisplayProtocol.swift
+++ Sources/Classes/core/Protocols/MiniAppDisplayProtocol.swift
@@ -9,7 +9,7 @@ public protocol MiniAppDisplayDelegate: AnyObject {
 
     /// Get the view of the Mini app
     func getMiniAppView() -> UIView
-    
+
     /// Interface  that will be used by the Host app to send Json
     func sendJsonToMiniApp(string jsonString: String)
 
