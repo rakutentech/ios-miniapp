6556959851aa9da50841b52e0586cf1e2a13f512
Tilak Gondi <115203844+GondiTilak@users.noreply.github.com>
2023-03-20 13:56:32 +0900

    [MINI-5999] Configure Demo app to support ATT (#488)

--- Example/SceneDelegate.swift
+++ Example/SceneDelegate.swift
@@ -1,9 +1,36 @@
 import UIKit
 import SwiftUI
+import AdSupport
+import AppTrackingTransparency
+import GoogleMobileAds
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // scene implementation
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            self.requestPermission()
+        }
+    }
+
+    func requestPermission() {
+        ATTrackingManager.requestTrackingAuthorization { status in
+            switch status {
+            case .authorized:
+                GADMobileAds.sharedInstance().start(completionHandler: nil)
+                print("Authorized Tracking Permission")
+            case .denied:
+                print("Denied Tracking Permission")
+            case .notDetermined:
+                print("Not Determined Tracking Permission")
+            case .restricted:
+                print("Restricted Tracking Permission")
+            @unknown default:
+                print("Unknown Tracking Permission")
+            }
+        }
     }
 }
 
