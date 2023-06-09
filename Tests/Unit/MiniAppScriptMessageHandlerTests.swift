be80cf53913cc8fa5d49aeeb70cbadf6109e63dd
Leo Joseph <leojoseph.r@rakuten.com>
2023-06-09 15:11:33 +0900

    Updated TC

--- Tests/Unit/MiniAppScriptMessageHandlerTests.swift
+++ Tests/Unit/MiniAppScriptMessageHandlerTests.swift
@@ -1535,7 +1535,7 @@ class MiniAppScriptMessageHandlerTests: QuickSpec {
                     expect(callbackProtocol.response).toEventually(equal("true"))
                 }
                 it("will return false if the device is NOT set to dark mode") {
-                    mockMessageInterface.mockIsDarkMode = true
+                    mockMessageInterface.mockIsDarkMode = false
                     let mockMessage = MockWKScriptMessage(name: "isDarkMode", body: "{\"action\":\"isDarkMode\",\"param\":null,\"id\":\"9.408169489793706\"}" as AnyObject)
                     scriptMessageHandler.userContentController(WKUserContentController(), didReceive: mockMessage)
                     expect(callbackProtocol.messageId).toEventually(equal("9.408169489793706"))
