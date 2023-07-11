75999affbe5820b1b78d694e245a773ad9510198
tilak.gondi <115203844+GondiTilak@users.noreply.github.com>
2023-07-11 14:19:54 +0900

    [No Ticket] Alert message pop up not seen in iOS 16+ (#507)
    * [No-Ticket] [Sample App] CI-Device build faling due to GoogleAppMeasurement framework _objc_msgSend errors.
    
    Adding the "-fno-objc-msgsend-selector-stubs" for other linker flags in MiniApp-Example target.
    
    * Revert "[No-Ticket] [Sample App] CI-Device build faling due to GoogleAppMeasurement framework _objc_msgSend errors."
    
    This reverts commit a4f2968e00224cdf1e7fcc138946c9838566faa2.
    
    * Xcode 14.1 changes
    
    * iPhone Simulator version
    
    * [No Ticket] Alert message pop up not seen in iOS 16+
    
    Fixed alert component issue in iOS 16+
    
    ---------
    
    Co-authored-by: Leo <leojoseph.r@rakuten.com>

--- Example/Controllers/SwiftUI/Features/Terms/MiniAppWithTermsView.swift
+++ Example/Controllers/SwiftUI/Features/Terms/MiniAppWithTermsView.swift
@@ -13,61 +13,113 @@ struct MiniAppWithTermsView: View {
     @State private var showJsonStringAlert: Bool = false
 
     var body: some View {
-        VStack {
-            switch viewModel.viewState {
-            case .none:
-                EmptyView()
-            case .loading:
-                ProgressView()
-            case let .permissionRequested(info, manifest):
-                MiniAppTermsView(
-                    didAccept: $didAcceptTerms,
-                    request: MiniAppPermissionRequest(sdkConfig: viewModel.sdkConfig, info: info, manifest: manifest)
-                )
-            case .error(let error):
-                Text(error.localizedDescription)
-                    .foregroundColor(Color(UIColor.secondaryLabel))
-                    .font(.system(size: 14, weight: .medium))
-                    .padding(.horizontal, 40)
-            case .offline:
-                MiniAppSUIView(params: miniAppViewParams(config: viewModel.sdkConfig), fromCache: true, handler: handler)
-            case .success:
-                MiniAppSUIView(params: miniAppViewParams(config: viewModel.sdkConfig), fromCache: false, handler: handler)
+        if #available(iOS 15.0, *) {
+            VStack {
+                switch viewModel.viewState {
+                case .none:
+                    EmptyView()
+                case .loading:
+                    ProgressView()
+                case let .permissionRequested(info, manifest):
+                    MiniAppTermsView(
+                        didAccept: $didAcceptTerms,
+                        request: MiniAppPermissionRequest(sdkConfig: viewModel.sdkConfig, info: info, manifest: manifest)
+                    )
+                case .error(let error):
+                    Text(error.localizedDescription)
+                        .foregroundColor(Color(UIColor.secondaryLabel))
+                        .font(.system(size: 14, weight: .medium))
+                        .padding(.horizontal, 40)
+                case .offline:
+                    MiniAppSUIView(params: miniAppViewParams(config: viewModel.sdkConfig), fromCache: true, handler: handler)
+                case .success:
+                    MiniAppSUIView(params: miniAppViewParams(config: viewModel.sdkConfig), fromCache: false, handler: handler)
+                }
+            }
+            .onChange(of: didAcceptTerms, perform: { accepted in
+                if accepted {
+                    viewModel.viewState = .success
+                }
+            })
+            .onReceive(viewModel.showMessageAlert) {
+                showMessageAlert = $0
+            }
+            .onChange(of: viewModel.viewState) { newValue in
+                switch newValue {
+                case .success:
+                    viewModel.addHandlerToList(handler)
+                default:
+                    print("No Handler to passs")
+                }
+            }.alert(Text(viewModel.showJsonString != nil ? "Universal Bridge" : "Info"), isPresented: $showMessageAlert) {
+                Button("OK") {
+                    viewModel.showMessageAlert.send(false)
+                }
+            } message: {
+                if let message = viewModel.showJsonString {
+                    Text(message)
+                } else {
+                    Text("Message sent!")
+                }
+                }
+        } else {
+            VStack {
+                switch viewModel.viewState {
+                case .none:
+                    EmptyView()
+                case .loading:
+                    ProgressView()
+                case let .permissionRequested(info, manifest):
+                    MiniAppTermsView(
+                        didAccept: $didAcceptTerms,
+                        request: MiniAppPermissionRequest(sdkConfig: viewModel.sdkConfig, info: info, manifest: manifest)
+                    )
+                case .error(let error):
+                    Text(error.localizedDescription)
+                        .foregroundColor(Color(UIColor.secondaryLabel))
+                        .font(.system(size: 14, weight: .medium))
+                        .padding(.horizontal, 40)
+                case .offline:
+                    MiniAppSUIView(params: miniAppViewParams(config: viewModel.sdkConfig), fromCache: true, handler: handler)
+                case .success:
+                    MiniAppSUIView(params: miniAppViewParams(config: viewModel.sdkConfig), fromCache: false, handler: handler)
+                }
+            }
+            .alert(isPresented: $showMessageAlert) {
+                if let message = viewModel.showJsonString {
+                    return Alert(
+                        title: Text("Universal Bridge"),
+                        message: Text(message),
+                        dismissButton: Alert.Button.cancel(Text("Ok"), action: {
+                            viewModel.showMessageAlert.send(false)
+                        }))
+                } else {
+                    return Alert(
+                        title: Text("Info"),
+                        message: Text("Message sent!"),
+                        dismissButton: Alert.Button.cancel(Text("Ok"), action: {
+                            viewModel.showMessageAlert.send(false)
+                        }))
+                }
+            }
+            .onChange(of: didAcceptTerms, perform: { accepted in
+                if accepted {
+                    viewModel.viewState = .success
+                }
+            })
+            .onReceive(viewModel.showMessageAlert) {
+                showMessageAlert = $0
+            }
+            .onChange(of: viewModel.viewState) { newValue in
+                switch newValue {
+                case .success:
+                    viewModel.addHandlerToList(handler)
+                default:
+                    print("No Handler to passs")
+                }
             }
         }
-        .alert(isPresented: $showMessageAlert) {
-            if let message = viewModel.showJsonString {
-                return Alert(
-                    title: Text("Universal Bridge"),
-                    message: Text(message),
-                    dismissButton: Alert.Button.cancel(Text("Ok"), action: {
-                        viewModel.showMessageAlert.send(false)
-                }))
-            } else {
-                return Alert(
-                    title: Text("Info"),
-                    message: Text("Message sent!"),
-                    dismissButton: Alert.Button.cancel(Text("Ok"), action: {
-                        viewModel.showMessageAlert.send(false)
-                }))
-            }
-        }
-        .onChange(of: didAcceptTerms, perform: { accepted in
-            if accepted {
-                viewModel.viewState = .success
-            }
-        })
-        .onReceive(viewModel.showMessageAlert) {
-            showMessageAlert = $0
-        }
-        .onChange(of: viewModel.viewState) { newValue in
-            switch newValue {
-            case .success:
-                viewModel.addHandlerToList(handler)
-            default:
-                print("No Handler to passs")
-            }
-        }
+
     }
 
     func miniAppViewParams(config: MiniAppSdkConfig) -> MiniAppViewParameters.DefaultParams {
